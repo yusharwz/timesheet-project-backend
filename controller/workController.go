b907b1d64e1e57b2261a5e4cb68357056cdfac09
yushar <yusharw@gmail.com>
2024-08-01 14:12:56 +0700

    feat:⚡️<FR0>solv conflict

--- controller/workController.go
+++ controller/workController.go
@@ -72,60 +72,6 @@ func (WorkController) DeleteWork(c *gin.Context) {
 
 	response.NewResponseSuccess(c, nil, "Work deleted successfully", "WORK", "200")
 }
-package controller
-
-import (
-	"final-project-enigma/dto/request"
-	"final-project-enigma/dto/response"
-	"final-project-enigma/service/impl"
-	"final-project-enigma/utils"
-	"github.com/gin-gonic/gin"
-)
-
-type WorkController struct{}
-
-var workService = impl.NewWorkService()
-
-func NewWorkController(g *gin.RouterGroup) {
-	controller := new(WorkController)
-	workGroup := g.Group("/work")
-	{
-		workGroup.POST("/", controller.CreateWork)
-		workGroup.GET("/", func(c *gin.Context) {
-			c.JSON(200, gin.H{"message": "Hello World!"})
-		})
-		workGroup.GET("/:id", controller.GetById)
-	}
-}
-
-func (WorkController) CreateWork(c *gin.Context) {
-	var workRequest request.WorkRequest
-	err := c.ShouldBindJSON(&workRequest)
-	if err != nil {
-		validationError := utils.GetValidationError(err)
-		response.NewResponseBadRequest(c, validationError, "Could not parse request", "", "")
-		return
-	}
-
-	result, err := workService.CreateWork(workRequest)
-	if err != nil {
-		response.NewResponseError(c, err.Error(), "", "")
-		return
-	}
-
-	response.NewResponseCreated(c, result, "Created new work successfully", "", "")
-
-}
-
-func (WorkController) GetById(c *gin.Context) {
-	id := c.Param("id")
-	result, err := workService.GetById(id)
-	if err != nil {
-		response.NewResponseError(c, err.Error(), "", "")
-		return
-	}
-	response.NewResponseSuccess(c, result, "Success fetch work data", "", "")
-}
 
 func (WorkController) GetById(c *gin.Context) {
 	id := c.Param("id")
