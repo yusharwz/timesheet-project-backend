2300600a206ba304a146aa3daa8495dc706efad2
yushar <yusharw@gmail.com>
2024-08-08 12:32:36 +0700

    fix:🐛bug on database update

--- entity/base.go
+++ entity/base.go
@@ -7,8 +7,8 @@ import (
 )
 
 type Base struct {
-	ID        string `gorm:"primaryKey"`
-	CreatedAt time.Time
+	ID        string    `gorm:"primaryKey"`
+	CreatedAt time.Time `gorm:"<-:create"`
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
